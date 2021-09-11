8739a6006b662ac3d5388c2ba92e96f9d78af82b
Yashi Chawla <yashichawla1@gmail.com>
2021-09-11 22:24:24 +0530

    fix: sol A1T2

--- US_Accidents/reducer_t2.py
+++ US_Accidents/reducer_t2.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import sys
-stateCount=dict()
+a=sys.stdin.readline()
+prev_state,prev_city,city_count=a.split(",")
+prev_state=prev_state.strip()
+prev_city=prev_city.strip()
+city_count=1
+state_count=1
+print(prev_state)
 for line in sys.stdin:
     state, city, count=line.split(",")
     state=state.strip()
@@ -10,17 +16,23 @@ for line in sys.stdin:
         count = int(count)
     except ValueError:
         continue
-    if state not in stateCount.keys():
-        stateCount[state]=dict()
-    if city not in stateCount[state].keys():
-        stateCount[state][city]=0
-    stateCount[state][city]+=1
 
-for state in sorted(list(stateCount.keys())):
-    print(state)
-    state_count=0
-    cityCount=stateCount[state]
-    for city in sorted(list(cityCount.keys())):
-        print(city,cityCount[city])
-        state_count+=cityCount[city]
-    print(state, state_count)
+    if prev_state==state:
+        if prev_city==city:
+            city_count+=1
+            state_count+=1
+        else: 
+            print(prev_city, city_count)
+            city_count=1
+            prev_city=city
+            state_count+=1
+    else: 
+        print(prev_state,state_count)
+        print(state)
+        state_count=1
+        prev_state=state
+        city_count=1
+        prev_city=city
+    
+print(city,city_count)
+print(state,state_count)
