c5065740aab2f9a2c8e3f207e5d31a9f009308e0
Yashi Chawla <yashichawla1@gmail.com>
2021-09-14 19:57:45 +0530

    fix: comment RT1

--- US_Accidents/reducer_t1.py
+++ US_Accidents/reducer_t1.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python3
 import sys
-# hourCount = dict()
-
-# for line in sys.stdin:
-#     hour, count = line.split(",")
-#     hour = int(hour.strip())
-#     count = count.strip()
-
-#     try:
-#         count = int(count)
-#     except ValueError:
-#         continue
-
-#     if hour not in hourCount.keys():
-#         hourCount[hour] = 0
-#     hourCount[hour] += count
-
-# for hour in sorted(list(hourCount.keys())):
-#     print(hour, hourCount[hour])
 
 hourCount=[0 for i in range(0,24)]
 for line in sys.stdin:
