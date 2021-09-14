a2034a585365bad95f1bd9dd0886054dd7c7111c
Yashi Chawla <yashichawla1@gmail.com>
2021-09-14 19:57:06 +0530

    fix: reducer T1

--- US_Accidents/reducer_t1.py
+++ US_Accidents/reducer_t1.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 import sys
-hourCount = dict()
+# hourCount = dict()
 
+# for line in sys.stdin:
+#     hour, count = line.split(",")
+#     hour = int(hour.strip())
+#     count = count.strip()
+
+#     try:
+#         count = int(count)
+#     except ValueError:
+#         continue
+
+#     if hour not in hourCount.keys():
+#         hourCount[hour] = 0
+#     hourCount[hour] += count
+
+# for hour in sorted(list(hourCount.keys())):
+#     print(hour, hourCount[hour])
+
+hourCount=[0 for i in range(0,24)]
 for line in sys.stdin:
     hour, count = line.split(",")
     hour = int(hour.strip())
@@ -12,9 +30,9 @@ for line in sys.stdin:
     except ValueError:
         continue
 
-    if hour not in hourCount.keys():
-        hourCount[hour] = 0
-    hourCount[hour] += count
+    hourCount[hour]+=1
 
-for hour in sorted(list(hourCount.keys())):
-    print(hour, hourCount[hour])
+for h in range(len(hourCount)):
+    if hourCount[h]>0:
+        print(h,hourCount[h])
+
