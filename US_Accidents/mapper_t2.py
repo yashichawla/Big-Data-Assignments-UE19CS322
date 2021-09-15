10245d56e1d95ce0641efcbe1bcec05f12664086
Yashi Chawla <yashichawla1@gmail.com>
2021-09-15 17:52:16 +0530

    fix: <=d, nan

--- US_Accidents/mapper_t2.py
+++ US_Accidents/mapper_t2.py
@@ -15,11 +15,12 @@ for line in sys.stdin:
     record=json.loads(line.strip())
     d_lat=record["Start_Lat"]
     d_lng=record["Start_Lng"]
-    coordinates={"latitude":d_lat,"longitude":d_lng}
-    distance=euclideanDistance(lat,lng,d_lat,d_lng)
-    if distance <d:
-        x=requests.post('http://20.185.44.219:5000/',json=coordinates)
-        #state city 1
-        new=x.json()
-        print("{},{},{:d}".format(new["state"],new["city"],1))
-    
+    if str(d_lat)!="nan" and str(d_lng)!="nan":
+        coordinates={"latitude":d_lat,"longitude":d_lng}
+        distance=euclideanDistance(lat,lng,d_lat,d_lng)
+        if distance <=d:
+            x=requests.post('http://20.185.44.219:5000/',json=coordinates)
+            #state city 1
+            new=x.json()
+            print("{},{},{:d}".format(new["state"],new["city"],1))
+        
