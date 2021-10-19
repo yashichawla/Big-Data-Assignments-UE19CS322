2aed01e0e076e00d7aeda841853e2aa7ce5972f8
Yashi-Chawla <yashichawla1@gmail.com>
2021-10-19 13:14:33 +0530

    Final A2

--- A2- PageRank/reducer_t2.py
+++ A2- PageRank/reducer_t2.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 import sys
-prev_page=0
-sum=0
-page=0
+prev_page = 0
+sum = 0
+page = 0
 for line in sys.stdin:
-    line=line.strip()
+    line = line.strip()
     try:
-        page, contribution=line.split(" ")
-        page=page.strip()
-        contribution=float(contribution.strip())
+        page, contribution = line.split(" ")
+        page = page.strip()
+        contribution = float(contribution.strip())
     except:
         continue
 
-    if prev_page==0:
-        prev_page=page
-        sum=contribution
+    if prev_page == 0:
+        prev_page = page
+        sum = contribution
 
-    elif prev_page==page:
-        sum+=contribution
+    elif prev_page == page:
+        sum += contribution
 
-    else: 
-        print("{},{:.2f}".format(prev_page,0.15+0.85*sum)
-        prev_page=page
-        sum=contribution
+    else:
+        print("{},{:.2f}".format(prev_page, 0.15+0.85*sum))
+        prev_page = page
+        sum = contribution
 
-print("{},{:.2f}".format(page,0.15+0.85*sum)
+print("{},{:.2f}".format(page, 0.15+0.85*sum))
