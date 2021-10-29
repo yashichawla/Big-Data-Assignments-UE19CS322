6c0fadcf60372f68afe478f6a667998dc5c55e57
yashi <yashichawla1@gmail.com>
2021-10-29 23:24:03 +0530

    add: A3T2

---
+++ A3- Spark/task2.py
@@ -0,0 +1,27 @@
+import sys
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.functions import count
+from pyspark.sql.functions import max
+spark_context = SparkContext.getOrCreate()
+sql_context = SQLContext(spark_context)
+
+path_city = sys.argv[1]
+path_global = sys.argv[2]
+
+df_city = sql_context.read.csv(path_city, header=True)
+df_global = sql_context.read.csv(path_global, header=True)
+
+df_country_max = df_city.groupBy(df_city.dt, df_city.Country).agg(
+    max(df_city.AverageTemperature).alias("MaxTemp"))
+df_join = df_country_max.join(df_global, ["dt"])
+df_filter = df_join.filter(df_join.MaxTemp > df_join.LandAverageTemperature)
+df_agg = df_filter.groupBy(df_filter.Country).agg(count(df_filter.Country))
+
+country_count = df_agg.collect()
+country_count = sorted(country_count, key=lambda x: list(x)[0])
+
+for row in country_count:
+    item = list(row)
+    country, count = item
+    print(f"{country}\t{count}")
